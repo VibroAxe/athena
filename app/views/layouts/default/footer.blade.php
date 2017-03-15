ab13f280aef4986207c8dffb2aa63e87efa68e7a
VibroAxe <vibs@macrolevel.co.uk>
2017-03-15 16:25:56 +0000

    LANPortal Branding
    Branded as LANPortal

--- app/views/layouts/default/footer.blade.php
+++ app/views/layouts/default/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<footer>
 		<p>
-			<a href="https://github.com/zeropingheroes/lanager" target="_blank">LANager</a> is licensed under <a href="http://www.gnu.org/licenses/agpl-3.0.html" target="_blank">AGPLv3</a>
+			<a href="http://lanportal.multiplay.co.uk">LANPortal</a> is a fork of <a href="https://github.com/zeropingheroes/lanager" target="_blank">LANager</a> and is licensed under <a href="http://www.gnu.org/licenses/agpl-3.0.html" target="_blank">AGPLv3</a>
 			<a href="http://steampowered.com" target="_blank" class="pull-right">Powered by Steam</a>
 		</p>
 	</footer>
