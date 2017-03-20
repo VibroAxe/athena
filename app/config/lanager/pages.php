c14935f6d7f2b0fc5f8b05ea12b3ebed87c37c56
VibroAxe <vibs@macrolevel.co.uk>
2017-03-20 17:30:46 +0000

    Added config option to display page titles
    set lanager/pages.showPageTitles to false to not display the page title
    on a page. This increases the flexibility of layout options

---
+++ app/config/lanager/pages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	///
+	/// Show page titles oo page rendering, 
+	/// If set to false the page will need to handle it's own title but this allows more flexibility on rendering
+	///
+	'showPageTitles' => true,
+];
+
+?>
--- app/views/pages/show.blade.php
+++ app/views/pages/show.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.default')
 @section('content')
+	@if ( Config::get('lanager/pages.showPageTitles',true) )
 	@include('layouts.default.title')
+	@endif
 	@include('layouts.default.alerts')
 
 	{{ Purifier::clean(Markdown::string($page->content), 'markdown') }}
