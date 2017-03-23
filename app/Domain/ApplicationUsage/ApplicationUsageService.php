b3f144a56f9bb09be2dc600d345c50c46b403da1
VibroAxe <vibs@macrolevel.co.uk>
2017-03-23 20:33:07 +0000

    Added caching onto application usage
    Adding caching onto application usage service for iseries
    
    Resolves #3

--- app/Domain/ApplicationUsage/ApplicationUsageService.php
+++ app/Domain/ApplicationUsage/ApplicationUsageService.php
@@ -3,6 +3,8 @@
 use Zeropingheroes\Lanager\Domain\States\StateService;
 use Zeropingheroes\Lanager\Domain\Applications\ApplicationTransformer;
 use Zeropingheroes\Lanager\Domain\Users\UserTransformer;
+use Config;
+use Cache;
 
 class ApplicationUsageService {
 
@@ -41,35 +43,48 @@ class ApplicationUsageService {
 	 */
 	public function userTotalsAt( $timestamp )
 	{
-		$states = $this->states->at( $timestamp )->whereNotNull('states.application_id')->get();
-
-		if ( count($states) )
-		{
-			// Collect and combine states for the same application
-			foreach($states as $state)
-			{
-				// merge states that refer to the same application 
-				$combinedUsage[$state->application_id]['application'] = $this->applicationTransformer->transform( $state->application );
-				
-				// add the state's user as a child of the above application key
-				$combinedUsage[$state->application_id]['users'][] = $this->userTransformer->transform( $state->user );
-			}
-
-			// Build clean array of applications
-			foreach($combinedUsage as $usageItem)
-			{
-				$usageItem['application']['users'] = $usageItem['users'];
-				$applications[] = $usageItem['application'];
-			}
-
-			// Sort applications array by user count, in decending order
-			usort($applications, function($a, $b) {
-				return count($b['users']) - count($a['users']);
-			});
-
-			return $applications;
+		//Note to future self:
+		//This is timestamp hash to correlate to the nearest steam polling interval
+		//In order to ensure that we have game date it returns the data for the previous polling period
+		$timestamp = $timestamp - $timestamp % 60;
+		if ($timestamp > time() - Config::get('lanager/steam.pollingInterval')) {
+			$timestamp = $timestamp - Config::get('lanager/steam.pollingInterval');
+			$timestamp = $timestamp - $timestamp % 60;
 		}
-		return [];
+		$applications= [];
+		if (Cache::has('applicationUsage'.$timestamp)) {
+			$applications =  Cache::get('applicationUsage'.$timestamp);
+		} else {
+			$states = $this->states->at( $timestamp )->whereNotNull('states.application_id')->get();
+	
+			if ( count($states) )
+			{
+				// Collect and combine states for the same application
+				foreach($states as $state)
+				{
+					// merge states that refer to the same application 
+					$combinedUsage[$state->application_id]['application'] = $this->applicationTransformer->transform( $state->application );
+				
+					// add the state's user as a child of the above application key
+					$combinedUsage[$state->application_id]['users'][] = $this->userTransformer->transform( $state->user );
+				}
+	
+				// Build clean array of applications
+				foreach($combinedUsage as $usageItem)
+				{
+					$usageItem['application']['users'] = $usageItem['users'];
+					$applications[] = $usageItem['application'];
+				}
+	
+				// Sort applications array by user count, in decending order
+				usort($applications, function($a, $b) {
+					return count($b['users']) - count($a['users']);
+				});
+	
+			}
+			Cache::add('applicationUsage'.$timestamp,$applications,1);
+		}
+		return $applications;
 	}
 
 }
